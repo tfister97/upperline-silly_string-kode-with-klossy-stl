644402a6521b26a4b06be0a9500b3f7e742a4efa
Theresa Fister <tfister97@gmail.com>
2016-06-26 01:36:05 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -2,23 +2,23 @@
 
 
 #1:  Create a string that contains your favorite word. It can be in any language!
+puts "Hufflepuff"
 
 #2: Using a method built into ruby, capitalize every letter in the string you just created
-
+puts "Hufflepuff".upcase
 
 #3: Now let's make all the characters lowercase in the string below:
 puts "IM NOT SHOUTING".downcase
 
 #4: Using a method built into ruby, count how many letters are in the string below:
-puts "supercalifragilisticexpialidocious"
+puts "supercalifragilisticexpialidocious".length
 
 #5: Let's add the two strings together below using concatenation.
-puts "wow" "mom"
+puts "wow" + "mom"
 
 #6. Let's capitalize the first character of the string below:
 
-puts "i really like programming"
+puts "i really like programming".capitalize
 
 #7 Replace all "A" with "O" and "a" with "o" by chaining together `gsub`
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
-
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub("A","O").gsub("a","o")
